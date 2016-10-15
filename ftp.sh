2ccc3b6c95a66f03ceac549805cb03fc01418a3f
Riccardo Crippa <therickys93@gmail.com>
2016-10-15 14:49:35 +0200

    added the file to clean the server and upload the files

---
+++ ftp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+rm composer.lock
+rm -Rf shippable/
+rm -Rf vendor/
+find . -name ".DS_Store" -print0 | xargs -0 rm -rf
+echo "setup some variables..."
+sleep 1
+SERVER= # Your server
+USER=  # Your username
+PASSW= # your password
+
+echo "connecting and deleting all files from server..."
+sleep 1
+ncftp <<END_OF_SESSION
+open -u $USER -p $PASSW $SERVER
+dir
+rm -rf *
+rm .htaccess
+dir
+put -R *
+put .htaccess
+dir
+rm -rf *.sh
+dir
+bye
+END_OF_SESSION
+sleep 1
+echo "Done!!!"
+sleep 1
